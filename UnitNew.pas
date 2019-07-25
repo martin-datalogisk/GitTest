3f01f89b98979350e6dd5c7e9b20dd7ce39a315b
martin.poulsen@datalogisk.dk <Martin_datalogisk>
2019-07-25 12:38:51 +0200

    Den 3. property

--- UnitNew.pas
+++ UnitNew.pas
@@ -9,14 +9,22 @@ type
   Nyklasse = class(TObject)
   private
     Fenparam: string;
+    Fen3param: word;
     procedure Setenparam(const Value: string);
+    procedure Seten3param(const Value: word);
     public
     property enparam : string read Fenparam write Setenparam;
+    property en3param : word read Fen3param write Seten3param;
   end;
 
 implementation
 
 { Nyklasse }
+
+procedure Nyklasse.Seten3param(const Value: word);
+begin
+  Fen3param := Value;
+end;
 
 procedure Nyklasse.Setenparam(const Value: string);
 begin
